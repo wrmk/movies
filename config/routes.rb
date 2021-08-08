5b613c14c93e4a36e2d631f961ede169e26592a7
wrmk <elmakan@gmail.com>
2021-08-08 19:44:19 +0300

    routes to categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'movies#index'
-  resources :movies
+  resources :movies do
+    collection do
+      # resources :category, only: [:index] do
+        # Movie.categories.map{|category| get category.downcase, to: 'movies#index'}
+        get 'category/:category', to: 'movies#index'
+      # end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
